e9bbb69853ef0df33dd5074950a58d308396227f
Deux Soto <deuxsoto@proton.me>
2025-04-08 12:02:05 +0200

    Apply gravity only in run and fall Actions

--- player/actions/action_run.gd
+++ player/actions/action_run.gd
@@ -1,1 +1,7 @@
 extends Action
+
+func _execute(delta: float) -> void:
+	if not player.is_on_floor():
+		player.gravity += player.up_direction * player_stats.gravity * delta
+	else:
+		player.gravity = Vector3.ZERO
