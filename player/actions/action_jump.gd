6f2af32cfe3588e6c29f1239ea52be10a072d40a
Deux Soto <deuxsoto@proton.me>
2025-04-09 12:07:51 +0200

    Restore half jump in Jump action
    (cherry picked from commit ca29621314cdf82a067d09a86936b16c882588fb)

--- player/actions/action_jump.gd
+++ player/actions/action_jump.gd
@@ -12,12 +12,14 @@ func _ready() -> void:
 func _enter() -> void:
 	# Reset gravity, especially for double jumping
 	player.gravity = Vector3.ZERO
-	#player.action_velocity += player.up_direction * player_stats.jump_strength
 	timer.wait_time = player_stats.jump_time
 	timer.start()
 
 func _execute(delta: float) -> void:
 	if not timer.is_stopped():
+		if Input.is_action_just_released("jump") and timer.time_left >= player_stats.half_jump_timer:
+			timer.start(player_stats.half_jump_timer)
+
 		var strength: float = remap(timer.time_left, player_stats.jump_time, 0.0, start_scale, end_scale)
 		player.action_velocity = player.up_direction * player_stats.jump_strength * strength
 	else:
