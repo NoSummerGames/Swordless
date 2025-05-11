cc9ac4d9633b1be798af4f321673e242ecd2f748
Deux Soto <deuxsoto@proton.me>
2025-05-12 00:14:13 +0200

    Make LevelResource contain also its level informations

--- hub/passages/src/passage.gd
+++ hub/passages/src/passage.gd
@@ -1,4 +1,6 @@
 class_name Passage
 extends HubObject
 
-@export var passage_resource: PassageResource
+@export var level: LevelResource
+@export var classic_level_scene: PackedScene
+@export var blackened_level_scene: PackedScene
