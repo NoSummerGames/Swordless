1e0faa93a947e36eaedd9427cc6bed333701aae8
Deux Soto <deuxsoto@proton.me>
2025-05-12 07:31:42 +0200

    Merge branch 'refs/heads/dev_dialogue_events' into dev_dialogue_editor

--- levels/src/resources/level_res.gd
+++ levels/src/resources/level_res.gd
@@ -9,3 +9,14 @@ extends Resource
 
 @export_group("Rules")
 @export var level_rules: GameRules
+
+@export_group("Infos")
+@export var name: StringName = ""
+@export_multiline var description: String
+@export var classic_level: StringName
+@export var blackened_level: StringName
+
+var discovered: bool = false
+var blackened_unlocked: bool = false
+var classic_finished: bool = false
+var blackened_finished: bool = false
