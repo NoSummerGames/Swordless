f8f613fcb0b5e0ec61a45cbf07e339fd10ec66a3
Deux Soto <deuxsoto@proton.me>
2025-05-14 15:02:54 +0200

    Merge changes

--- player/components/player_animation_component.gd
+++ player/components/player_animation_component.gd
@@ -2,7 +2,6 @@ extends AnimatedSprite3D
 
 @export var commands_controller: CommandController
 
-
 func _ready() -> void:
 	commands_controller.command_entered.connect(_on_command_entered)
 	commands_controller.command_exited.connect(_on_command_exited)
@@ -11,28 +10,32 @@ func _ready() -> void:
 
 func _on_command_entered(command: Command) -> void:
 	if command in commands_controller.active_commands:
-		if try_playing(command.enter_animation):
+		if play_animation("enter_animation"):
 			await animation_finished
-			if animation != command.enter_animation:
-				return
 
-		try_playing(command.animation)
+		play_animation("animation")
 
 	else:
 		if is_playing():
 			await animation_finished
-			try_playing(command.animation)
+		play_animation(command.animation)
 
 func _on_command_exited(command: Command) -> void:
 		if sprite_frames.has_animation(command.exit_animation):
-			play(command.exit_animation)
+			play_animation(command.exit_animation)
 
-func try_playing(animation_name: String) -> bool:
-	if sprite_frames.has_animation(animation_name):
-		play(animation_name)
-		return true
+func play_animation(property: String) -> bool:
+	var desired_animation: String = commands_controller.get(property)
+
+	if not property == null:
+		if sprite_frames.has_animation(desired_animation):
+			play(desired_animation)
+			return true
+		else:
+			printerr(desired_animation + " animation couldn't be found.")
 	else:
-		return false
+		printerr(property + " player property couldn't be found.")
+	return false
 
 func _on_animation_changed() -> void:
 	print(animation)
